ca4f3d99a10cefe1ae236ca8420795d51b6b9d88
parkjoo1 <parkjoo1@students.zhaw.ch>
2022-10-22 10:31:36 +0200

    build rest api for ads card & refacorting

---
+++ client/src/components/Test.js
@@ -0,0 +1,17 @@
+const Test = ({ images }) => {
+  return (
+    <div>
+      {images.map((img) => (
+        <div key={img}>
+          <img
+            src={`http://localhost:3001/api/items/images/${img}`}
+            alt="example"
+          />
+          <p>{img}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Test
--- controllers/items.js
+++ controllers/items.js
@@ -1,12 +1,65 @@
-const itemsRouter = require('express').Router()
-const Item = require('../models/item')
+const itemsRouter = require('express').Router();
+const Item = require('../models/item');
 
+const imageHelper = require('../utils/imageHelper');
+const config = require('../utils/config')
 
-itemsRouter.get('/', async (req, res) => {
-  const items = await Item
-    .find({})
+const mongoose = require("mongoose");
+const url = config.MONGODB_URI
 
-    res.json(items)
+const conn = mongoose.createConnection(url, { useNewUrlParser: true, useUnifiedTopology: true });
+let gfsBucket
+conn.once("open", () => {
+  gfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+    bucketName: 'imageUploads',
+  })
 })
 
-module.exports = itemsRouter
+itemsRouter.get('/', async (req, res) => {
+  const items = await Item.find({});
+
+  res.json(items);
+});
+
+itemsRouter.post(
+  '/',
+  imageHelper.upload.single('file'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      console.log(req.file);
+      const newItem = new Item({
+        title: body.title,
+        description: body.description,
+        owner: 'empty',
+        price: body.price,
+        filename: req.file.filename,
+        fileId: req.file.id,
+      });
+
+      const savedNewItem = await newItem.save();
+      res.status(201).json(savedNewItem);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+itemsRouter.get('/images', async (req, res) => {
+  const items = await Item.find({});
+  const fileIDs = items.map(img => img.fileId)
+
+  res.status(201).json(fileIDs);
+})
+
+itemsRouter.get('/images/:id', async (req, res, next) => {
+  try{
+    const id = req.params.id
+    gfsBucket.openDownloadStream(new mongoose.Types.ObjectId(id)).pipe(res);
+  } catch(e) {
+    next(e)
+  }
+
+})
+
+module.exports = itemsRouter;
