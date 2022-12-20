cb58abbcb2b0cfb69f0a975d8c4c706356d5f557
Amin405 <amin_soussi_github@hotmail.com>
2022-12-20 14:56:39 +0100

    fixed login err message

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -17,9 +17,14 @@ const Login = () => {
       email: e.target.email.value,
       password: e.target.password.value,
     };
-    const res = await authService.login(credentials);
-    dispatch(setUser(res));
-    navigate('/');
+    try {
+      const res = await authService.login(credentials);
+      dispatch(setUser(res));
+      navigate('/');
+    } catch (err) {
+      setLoginAttempt(true)
+      console.log(err.message)
+    }
   };
 
   return (
@@ -92,7 +97,6 @@ const Login = () => {
             {/*SignIn Button */}
             <div>
               <button
-                onClick={() => setLoginAttempt(true)}
                 type="submit"
                 className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
